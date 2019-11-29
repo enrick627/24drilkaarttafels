6fd5ea6520f90983ca5ce32e9b2b706d322b4a81
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-11-29 09:56:16 +0100

    2de commit

--- drilkaarttafels/Program.cs
+++ drilkaarttafels/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿//Enrick De Munter nr3 5ITN 29/11/19
+/* OPGAVE
+ * ======
+ * 1)TVV V5
+ * 2)TVV V getallen ingegeven door user
+ * 3)2x TVV V "                     "
+ * 4) OPGAVE ZELF
+ * 
+ * 
+ *ANALYSE
+ * VRAAG GEEF DE TAFELS VAN 5
+ * 
+ * PROCESSING
+ * BEREKEN ALLE TAFELS VAN 5
+ * 
+ * TOON ALLE TAFELS VAN 5
+ */
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +30,28 @@ namespace drilkaarttafels
     {
         static void Main(string[] args)
         {
+            
+
+
+
+            Console.WriteLine(5 * 1);
+            Console.WriteLine(5 * 2);
+            Console.WriteLine(5 * 3);
+            Console.WriteLine(5 * 4);
+            Console.WriteLine(5 * 5);
+            Console.WriteLine(5 * 6);
+            Console.WriteLine(5 * 7);
+            Console.WriteLine(5 * 8);
+            Console.WriteLine(5 * 9);
+            Console.WriteLine(5 * 10);
+
+
+
+
+            //wachten.....
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten");
+            Console.ReadKey();
         }
     }
 }
